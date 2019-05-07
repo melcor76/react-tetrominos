62bcccfdeb278ef65c45926274245c2606f6136e
Michael Karén <michael.karen@storebrand.no>
2019-05-07 11:10:29 +0200

    handle playing change

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,25 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, FunctionComponent } from 'react';
 import './game.css';
 import Board from '../Board';
 
 const Game = () => {
+  const [playing, setPlaying] = useState(false);
 
-  const PlayButton = () => {
-    const [playing, setPlaying] = useState(false);
-  
-    const handleClick = () => {
-      setPlaying(!playing);
-    };
-  
-    return  <button onClick={handleClick}>{playing ? 'Stop' : 'Play'}</button>
-  }
+  const handlePlayingChange = () => {
+    if (playing) {
+      setPlaying(false);
+      console.log('game over!');
+    } else {
+      setPlaying(true);
+      console.log('game on!');
+    }
+  } 
 
-    return (
-      <div className="game">
-        <Board width={300} height={600} />
-        <PlayButton />
-      </div>
-    )
+  return (
+    <div className="game">
+      <Board width={300} height={600} />
+      <PlayButton playing={playing} onChangePlaying={handlePlayingChange} />
+    </div>
+  )
 }
+
+const PlayButton:FunctionComponent<{playing: boolean, onChangePlaying: any}> = ({playing, onChangePlaying}) => (
+  <button onClick={onChangePlaying}>{playing ? 'Stop' : 'Play'}</button>
+);
 
 export default Game;
